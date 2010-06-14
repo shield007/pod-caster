f15d730e524c859568bd856cb17e63dc07e0747d
jp.stanford <jp.stanford@0f0b96c8-6a32-11de-859f-df06e5afd5d7>
2010-06-14 07:29:33 +0000

    Started to use jaudiotagger version 2.0.2 instead of older version

--- PodCaster/src/org/stanwood/podcaster/config/Config.java
+++ PodCaster/src/org/stanwood/podcaster/config/Config.java
@@ -7,8 +7,8 @@ public class Config {
 
 	private static Config instance = null;
 
-	private final static String DEFAULT_FFMPEG_PATH = "/usr/bin/ffmpeg";
-	private final static String DEFAULT_MPLAYER_PATH = "/usr/bin/mplayer";
+	private final static String DEFAULT_FFMPEG_PATH = File.separator+"usr"+File.separator+"bin"+File.separator+"ffmpeg";
+	private final static String DEFAULT_MPLAYER_PATH = File.separator+"usr"+File.separator+"bin"+File.separator+"mplayer";
 
 	private File ffmpegPath = new File(DEFAULT_FFMPEG_PATH);
 	private File mplayerPath = new File(DEFAULT_MPLAYER_PATH);
