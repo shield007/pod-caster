a818ddc7dd2e8108311bc9ac4915640f6f3d77d8
jp.stanford <jp.stanford@0f0b96c8-6a32-11de-859f-df06e5afd5d7>
2011-10-19 15:34:02 +0000

    Added test

---
+++ PodCaster/tests/org/stanwood/podcaster/audio/TestMP3File.java
@@ -0,0 +1,32 @@
+package org.stanwood.podcaster.audio;
+
+import java.io.File;
+
+import org.junit.Test;
+import org.stanwood.podcaster.util.FileHelper;
+
+public class TestMP3File {
+
+	@Test
+	public void testWriteMetaData() throws Exception {
+		File tmpMp3 = FileHelper.createTempFile("test", ".mp3");
+		try {
+			if (tmpMp3.exists()) {
+				FileHelper.delete(tmpMp3);
+			}
+			FileHelper.copy(TestMP3File.class.getResourceAsStream("test.mp3"),tmpMp3 );
+
+			MP3File mp3file = new MP3File(tmpMp3);
+			mp3file.setArtist("Test");
+			mp3file.setCopyright("Blah");
+			mp3file.setDescription("Blah Blah Blah");
+			mp3file.setTitle("A title");
+			mp3file.writeMetaData();
+		}
+		finally {
+			if (tmpMp3.exists()) {
+				FileHelper.delete(tmpMp3);
+			}
+		}
+	}
+}
